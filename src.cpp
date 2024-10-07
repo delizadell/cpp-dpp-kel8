602d19afee0a817bf9e94ba4154a442ffeec2f67
Kalri <53751770+KhairiFadhil@users.noreply.github.com>
2024-10-07 11:25:17 +0700

    Update src.cpp
    Minor Change

--- src.cpp
+++ src.cpp
@@ -25,20 +25,21 @@ bool login(string username, string password, const vector<Account>& accounts) {
     return false;
 }
 
-void registerAccount(string username, string password, vector<Account>& accounts) {
-    for (const Account& account : accounts) {
+void registerAccount(string username, string password, vector<Account>& verif) {
+    for (const Account& account : verif) {
         if (username == account.username) {
             cout << "Registrasi Gagal, Akun sudah terdaftar." << endl;
             return; 
         }
     }
 
-    accounts.push_back({username, password});
+    verif.push_back({username, password});
     cout << "Registration successful!" << endl;
 }
 
 int main() {
     vector<Account> accounts = {{"admin", "admin123"}}; 
+    vector<Account> verif = {};
     string username, password;
     string regUsername, regPassword;
     bool isLogged = false; 
@@ -54,7 +55,7 @@ int main() {
             cin >> regUsername;
             cout << "Password: ";
             cin >> regPassword;
-            registerAccount(regUsername, regPassword, accounts); 
+            registerAccount(regUsername, regPassword, verif); 
         } else if (n == 2) { 
             while (!isLogged) {
                 cout << "Username: ";
@@ -76,6 +77,13 @@ int main() {
             cout << "========================================" << endl;
             cout << "User: " << username << endl;
             cout << "9: Log Out" << endl; 
+            cout << "8. Account Approval";
+            if(verif.size()  > 0){
+                cout << endl;
+                cout << "(Terdapat "<< verif.size() << " " << "akun menunggu Verifikasi)" << endl;
+            }else{
+                cout << endl;
+            }
             cout << "========================================" << endl;
 
 
@@ -87,7 +95,29 @@ int main() {
 
 
             cin >> input;
-
+            if(input == 8){
+                int acc;
+                vector<Account>temp = {};
+                char yesno;
+            cout << "=========" << "Account Approval" << " =========" << endl << endl;
+                for (int i = 0; i < verif.size(); i++) {
+                    cout << i + 1 << ". " << "Username: " << verif[i].username <<  ", Password: " << verif[i].password << endl;
+                }
+                cin >> acc;
+                char con;
+                for (int i = 0; i < verif.size(); i++) {
+                    if(i == acc){
+                    cout << "Apakah kau yakin ingin menambahkan user:" << verif[i].username << "? [y/n]" << endl;
+                    cin >> con;
+                    if(con == 'y'){
+                        accounts.push_back({verif[i].username, verif[i].password});
+                        break;
+                    }else{
+                        break;
+                    }
+                    }
+                 }
+            }
             if (input == 9) {
                 char confirm;
                 cout << "Apa kau yakin ingin Logout? (y/n): ";
